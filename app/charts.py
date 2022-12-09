c7543a286ddd11550e2bc09843ee3e77092a8753
jhon954 <jhon.moreno451@gmail.com>
2022-12-09 18:26:27 -0500

    Final Docker

--- app/charts.py
+++ app/charts.py
@@ -11,7 +11,7 @@ def generate_pie_chart(labels, values):
   fig,ax=plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig('pie_app.png')
+  plt.savefig('chart_pie_app_final.png')
   plt.close()
 if __name__ == '__main__':
   labels=['a','b','c']
